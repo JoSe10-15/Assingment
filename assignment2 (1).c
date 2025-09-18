c7a7b09bbaa8df7b569b81db7b245ae24f87ce95
JoSe10-15 <maxgusseyn@gmail.com>
2025-09-18 10:31:26 +0300

    Add files via upload

---
+++ assignment2 (1).c
@@ -0,0 +1,28 @@
+// Assignment 2
+
+/*
+Name:Joseph Mokua
+Reg no:PA106/G/28832/25
+Description:C program requiring user to enter his/her details
+*/
+
+#include<stdio.h>
+
+//main function 
+int main(){
+     int height;
+     printf("Enter your height:");
+     scanf("%d",&height);
+     printf("Your height is%d\n",height);
+     int IDnumber;
+     printf("Enter your IDnumber:");
+     scanf("%d",&IDnumber);
+     printf("Your IDnumber is %d\n",IDnumber);
+     int Bankbalance;
+     printf("Enter your Bankbalance:");
+     scanf("%d",Bankbalance);
+     printf("Your bankbalance is %d\n",Bankbalance);
+     
+     return 0;
+     
+}
---
+++ units 1.1.c
@@ -0,0 +1,23 @@
+// Assignment 1
+
+/*
+Name:Joseph Mokua
+Reg no:PA106/G/28832/25
+Description:C program displaying semester one units
+*/
+
+#include<stdio.h>
+
+//main function 
+int main(){
+     printf("Introduction to programming and problem solving\n");
+     printf("Fundamentals of ICT\n");
+     printf("Communication skills\n");
+     printf("Discrete Mathematics\n");
+     printf("Mathematics for science\n");
+     printf("Computer system and organization\n");
+     printf("Fundamentals of System Analysis and Design\n");
+     
+     return 0;
+     
+}
